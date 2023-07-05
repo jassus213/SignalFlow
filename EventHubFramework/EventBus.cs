677b0bedad8f69a11bf275de8b7e35554a85310f
Nikita Okhotnikov <nikitahunter23@gmail.com>
2023-07-05 23:44:07 +0300

    Async Signals

--- EventHubFramework/EventBus.cs
+++ EventHubFramework/EventBus.cs
@@ -1,4 +1,5 @@
-﻿using EventHubFramework.Common;
+﻿using System.Collections.Concurrent;
+using EventHubFramework.Common;
 using EventHubFramework.Signal;
 using EventHubFramework.Signal.Factory;
 
@@ -6,9 +7,20 @@ namespace EventHubFramework;
 
 public static class EventBus
 {
-    private static readonly Dictionary<Type, object> _map = new Dictionary<Type, object>();
+    private static readonly ConcurrentDictionary<Type, object> _map = new ConcurrentDictionary<Type, object>();
 
     public static void Subscribe<TSignal>(this object subscriber, Action<TSignal> method)
+        where TSignal : Signal<TSignal>
+    {
+        var type = typeof(TSignal);
+        if (!_map.ContainsKey(type))
+            throw new EventBusException($"Cant Find Current Signal {typeof(TSignal)}");
+
+        var signal = _map[type] as Signal<TSignal>;
+        signal.Subscribe(subscriber, method);
+    }
+
+    public static void Subscribe<TSignal>(this object subscriber, Func<TSignal, Task> method)
         where TSignal : Signal<TSignal>
     {
         var type = typeof(TSignal);
@@ -29,11 +41,21 @@ public static class EventBus
         signal.UnSubscribe(subscriber, method);
     }
 
+    public static void UnSubscribe<TSignal>(this object subscriber, Func<TSignal, Task> method)
+    {
+        var type = typeof(TSignal);
+        if (!_map.ContainsKey(type))
+            throw new EventBusException($"Cant Find Current Signal {typeof(TSignal)}");
+
+        var signal = _map[type] as Signal<TSignal>;
+        signal.UnSubscribe(subscriber, method);
+    }
+
     public static TSignal RegisterSignal<TSignal>() where TSignal : Signal<TSignal>
     {
         var type = typeof(TSignal);
         var signal = SignalFactory.Create<TSignal>().Invoke();
-        _map.Add(type, signal);
+        _map.TryAdd(type, signal);
         return signal;
     }
 
@@ -75,5 +97,27 @@ public static class EventBus
 
         var signal = _map[type] as Signal<TSignal>;
         signal.Fire(currentSignal.Invoke());
+    }
+
+    public static async Task FireAsync<TSignal>() where TSignal : Signal<TSignal>
+    {
+        var type = typeof(TSignal);
+        if (!_map.ContainsKey(type))
+            throw new EventBusException($"Cant Find Current Signal {typeof(TSignal)}");
+
+
+        var signal = _map[type] as Signal<TSignal>;
+        await signal.FireAsync();
+    }
+
+    public static async Task FireAsync<TSignal>(Func<TSignal> currentSignal) where TSignal : Signal<TSignal>
+    {
+        var type = typeof(TSignal);
+        if (!_map.ContainsKey(type))
+            throw new EventBusException($"Cant Find Current Signal {typeof(TSignal)}");
+
+
+        var signal = _map[type] as Signal<TSignal>;
+        await signal.FireAsync(currentSignal.Invoke());
     }
 }
--- EventHubFramework/Signal/Signal.cs
+++ EventHubFramework/Signal/Signal.cs
@@ -4,40 +4,63 @@ public abstract class Signal<TSignal>
 {
     private readonly TSignal _currentSignal;
 
-    private readonly Dictionary<Type, KeyValuePair<object, Action<TSignal>>> Subscribers =
+    private readonly Dictionary<Type, KeyValuePair<object, Action<TSignal>>> _subscribers =
         new Dictionary<Type, KeyValuePair<object, Action<TSignal>>>();
+
+    private readonly Dictionary<Type, KeyValuePair<object, Func<TSignal, Task>>> _queueAsyncSubscribers =
+        new Dictionary<Type, KeyValuePair<object, Func<TSignal, Task>>>();
 
     internal void Subscribe(object subscriber, Action<TSignal> action)
     {
         var type = subscriber.GetType();
-        if (Subscribers.ContainsKey(type))
+        if (_subscribers.ContainsKey(type))
             throw new Exception($"Already Subscribed {type}");
 
-        Subscribers.Add(type, new KeyValuePair<object, Action<TSignal>>(subscriber, action));
+        _subscribers.Add(type, new KeyValuePair<object, Action<TSignal>>(subscriber, action));
+    }
+    
+    
+    internal void Subscribe(object subscriber, Func<TSignal, Task> action)
+    {
+        var type = subscriber.GetType();
+        if (_subscribers.ContainsKey(type))
+            throw new Exception($"Already Subscribed {type}");
+
+        _queueAsyncSubscribers.Add(type, new KeyValuePair<object, Func<TSignal, Task>>(subscriber, action));
     }
 
     internal void UnSubscribe(object subscriber)
     {
         var type = subscriber.GetType();
-        if (!Subscribers.ContainsKey(type))
+        if (!_subscribers.ContainsKey(type))
             throw new Exception($"Not Subscribed {type}");
 
-        Subscribers.Remove(type);
+        _subscribers.Remove(type);
     }
 
     internal void UnSubscribe(object subscriber, Action<TSignal> method)
     {
         var type = subscriber.GetType();
-        if (!Subscribers.ContainsKey(type))
+        if (!_subscribers.ContainsKey(type))
             throw new Exception($"Not Subscribed {type}");
 
-        if (Subscribers[type].Value == method)
-            Subscribers.Remove(type);
+        if (_subscribers[type].Value == method)
+            _subscribers.Remove(type);
+    }
+    
+    internal void UnSubscribe(object subscriber, Func<TSignal, Task> method)
+    {
+        var type = subscriber.GetType();
+        if (!_subscribers.ContainsKey(type))
+            throw new Exception($"Not Subscribed {type}");
+
+        if (_queueAsyncSubscribers[type].Value == method)
+            _queueAsyncSubscribers.Remove(type);
     }
 
     internal void Fire()
     {
-        foreach (var subscriber in Subscribers.Values)
+        foreach (var subscriber in _subscribers.Values)
         {
             subscriber.Value.Invoke(_currentSignal);
         }
@@ -45,9 +68,25 @@ public abstract class Signal<TSignal>
 
     internal void Fire(TSignal currentSignal)
     {
-        foreach (var subscriber in Subscribers.Values)
+        foreach (var subscriber in _subscribers.Values)
         {
             subscriber.Value.Invoke(currentSignal);
+        }
+    }
+    
+    internal async Task FireAsync()
+    {
+        foreach (var subscriber in _queueAsyncSubscribers.Values)
+        {
+            await subscriber.Value.Invoke(_currentSignal);
+        }
+    }
+
+    internal async Task FireAsync(TSignal currentSignal)
+    {
+        foreach (var subscriber in _queueAsyncSubscribers.Values)
+        {
+            await subscriber.Value.Invoke(currentSignal);
         }
     }
 }
